97b8ee880a38dd296f1e5fa6c85b604764882ddd
dsstewa <davidstewart@gmail.com>
2019-08-06 22:15:23 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  # get 'songs/show'
+
+  # get 'songs/create'
+
+  # get 'songs/new'
+
+  # get 'songs/edit'
+
+  # get 'songs/index'
+
+  resources :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
