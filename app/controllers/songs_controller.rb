308951ad7a61fc8f41286c63b30997951e7af00c
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-08-14 14:32:31 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,48 @@
+class SongsController < ApplicationController
+  
+  def index
+    @song = Song.all
+  end
+  
+  def create 
+    @song = Song.new(song_params)
+    
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def update
+        @song = Song.find(params[:id])
+    if @song.update(song_params)
+    redirect_to post_path(@song)
+    else
+    render :edit
+    end
+  end
+  
+  def delete 
+  end
+  
+  def edit
+    @song = Song.find(params[:id])
+  end
+  
+  def show 
+    @song = Song.find(params[:id])
+  end
+  
+  def new
+    @song = Song.new
+  end 
+  
+  private
+
+  def song_params
+    params.permit(:title, :release_year, :artist_name, :genre)
+  end
+  
+end
