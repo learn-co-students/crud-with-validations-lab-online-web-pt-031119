0d96c57ccbf0ae8cf4caa777900058646c8e729b
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-08-14 15:52:02 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,7 +6,6 @@ class SongsController < ApplicationController
   
   def create 
     @song = Song.new(song_params)
-    
     if @song.valid?
       @song.save
       redirect_to song_path(@song)
@@ -16,9 +15,9 @@ class SongsController < ApplicationController
   end
 
   def update
-        @song = Song.find(params[:id])
+    @song = Song.find(params[:id])
     if @song.update(song_params)
-    redirect_to post_path(@song)
+    redirect_to song_path(@song)
     else
     render :edit
     end
@@ -42,7 +41,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.permit(:title, :release_year, :released, :artist_name, :genre)
+    params.require(:song).permit(:title, :release_year, :released, :artist_name, :genre)
   end
   
 end
